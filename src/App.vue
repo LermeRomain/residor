0c2dc102a2c3c9b038ba5edd838e7bc7b987e47b
romai <romainlerme@hotmail.fr>
2023-08-24 16:37:07 +0200

    test affichage 3

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <div id="guestready-booking-widget__root"></div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import { onMounted } from 'vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
-  },
-  setup() {
-    onMounted(() => {
-      const script = document.createElement('script');
-      script.src = 'https://book.guestready.com/booking_widget/main.js';
-      script.async = true;
-      document.head.appendChild(script);
-    });
-
-    return {}; // Assurez-vous de renvoyer un objet ou des valeurs si nécessaire
   }
 }
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,29 @@
 <template>
-  <div class="hello">
-
+  <div id="guestready-booking-widget__root"
+       data-rental="72410"
+       data-locale="fr"
+       data-reservation-platform-id="132"
+       data-base-url="https://openflats.rentalready.io"
+  >
   </div>
 </template>
 
 <script>
+import { onMounted } from 'vue';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+    setup() {
+      onMounted(() => {
+        const script = document.createElement('script');
+        script.src = 'https://book.guestready.com/booking_widget/main.js';
+        script.async = true;
+        document.head.appendChild(script);
+      });
+
+      return {}; // Assurez-vous de renvoyer un objet ou des valeurs si nécessaire
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
